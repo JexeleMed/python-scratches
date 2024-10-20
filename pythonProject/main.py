efe83eccfd0e6e7bfc6b67c19c53462c99889a92
Kuba Majer <jexelem@gmail.com>
2024-10-20 11:46:53 +0200

    First scratch

---
+++ pythonProject/main.py
@@ -0,0 +1,19 @@
+def dodawanie(a, b):
+    return a+b
+
+def odejmowanie(a,b):
+    return a-b
+
+def mnozenie(a, b):
+    return a*b
+
+def dzielenie(a,b):
+    if b==0:
+        print("Nie mozna dzielic przez zero")
+    else:
+        return a/b
+
+
+
+
+
